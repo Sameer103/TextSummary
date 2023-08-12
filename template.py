9ac66c7c96408058b81c23536583c9ae3238773d
Sameer103 <79912125+Sameer103@users.noreply.github.com>
2023-08-12 19:53:48 +0530

    template

--- template.py
+++ template.py
@@ -6,7 +6,8 @@ logging.basicConfig(level= logging.INFO, format='[%(asctime)s]: %(message):')
 
 project_name= "TextSummarizer"
 list_of_files = [
-    ".github/workflows/.gitkeep",
+   ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
     f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
@@ -27,17 +28,22 @@ list_of_files = [
 
 ]
 
+
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
     
-    if filedir != "" :
-       os.makedirs(filedir, exist_ok=True)
-       logging.info(f"Creating Directory: {filedir} for the file {filename}")
-       
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filename, 'w') as f:
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
             pass
-            logging.info(f"Creating File: {filename}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
         logging.info(f"{filename} is already exists")
